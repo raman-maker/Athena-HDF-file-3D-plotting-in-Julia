8d24415acdb08e5ae8f22dd537ef262b522d2712
Raman Kumar <64368013+raman-maker@users.noreply.github.com>
2025-08-28 15:29:58 +0200

    mesh volume count
    added a function that calculates the total volume of mesh.

--- Mesh.jl
+++ Mesh.jl
@@ -4,18 +4,15 @@ function plothdf(h5file::String)
 	h5 = h5open(h5file, "r")
 	num_blocks::Int32 = read(HDF5.attributes(h5),"NumMeshBlocks")
 	MeshBlockSize::Vector{Int32} = read(HDF5.attributes(h5),"MeshBlockSize")
-	Bx = read(h5["B"])[:,:,:,:,1]
-	By = read(h5["B"])[:,:,:,:,2]
-	Bz::Array{Float32, 4} = read(h5["B"])[:,:,:,:,3]
-	Bt = similar(Bx)
-	Bt::Array{Float32, 4} = @. sqrt(Bx^2 + By^2 + Bz^2)
+	Br = read(h5["B"])[:,:,:,:,1]
+	Bθ = read(h5["B"])[:,:,:,:,2]
+	Bϕ::Array{Float32, 4} = read(h5["B"])[:,:,:,:,3]
+	Bp = @. hypot(Br, Bθ)/(Bϕ)
 	x_all::Array{Float32, 2} = read(h5["x1f"])   # shape: (Nx, Nblocks)
 	y_all::Array{Float32, 2} = (read(h5["x2f"]))
 	z_all::Array{Float32, 2} = (read(h5["x3f"]))
 	close(h5)
 	fig = Figure()
-	pl = PointLight(RGBf(20, 20, 20), Point3f(0))
-	al = AmbientLight(RGBf(0.2, 0.2, 0.2))
 	ax::LScene = LScene(fig[1, 1], show_axis=false)
 
 	function to_vertex_centered(Bt_block::Array{Float32,3})
@@ -33,23 +30,31 @@ function plothdf(h5file::String)
 		end
 		Bt_vertex ./= 8
 	end
-
-    for b in num_blocks
+    #cr = log10.(extrema(abs.(Bp)))
+    vlm = 0.0
+    for b in 1:num_blocks
 		print("\rBlock: $b ")
-		Bt_block = Bt[:, :, :, b]
-		Bt_vertex = to_vertex_centered(Bt_block)
-		nrho = log10.(abs.(vec(Bt_vertex)))
-		r = x_all[:, b]
-		θ = y_all[:, b]
-		ϕ = z_all[:, b]
-		g = RectilinearGrid{𝔼,typeof(Spherical(0,0,0))}((r, θ, ϕ))
-		#m = mapreduce(boundary, merge, g) |> Repair(0)
-		@show size(nrho)
-		@show nelements(g)
-		@show nvertices(g)
-		viz!(g, color = nrho, alpha=0.9)
+		try
+			Bp_block = Bp[:, :, :, b]
+			Bp_vertex = to_vertex_centered(Bp_block)
+			nrho = abs.(vec(Bp_vertex))
+			cre = extrema(nrho)
+			r = x_all[:, b]
+			θ = y_all[:, b]
+			ϕ = z_all[:, b]
+			g = RectilinearGrid{𝔼,typeof(Spherical(0,0,0))}((r, θ, ϕ))
+			vlm += measure(g).val
+			m = mapreduce(boundary, merge, g) |> Repair(0)
+			viz!(m, color = nrho, alpha=0.1, colorrange = cre, colormap =:hawaii)
+		catch e
+		    if isa(e, ArgumentError)
+		    	printstyled("ArgumentError in Block: $b \n", blink=true, color=:light_red)
+		    else 
+		    	printstyled("$e in Block: $b \n", blink=true, color=:light_red)
+		    end
+        end # trycatch
 	end
+	@show vlm
 	fig
 end
-@timev plothdf("sane00.prim.01800.athdf")
-
+@time plothdf("sane00.prim.01800.athdf")
